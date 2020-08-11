49c0af9035bbc6130e9d6c691c25ad7fad3e6bde
sigongweb <kisssulran@i-screammedia.com>
2020-08-11 10:03:18 +0900

    Add comment about "Rule -Name option"

--- ADD_and_Update_Resource/Update_NSG.ps1
+++ ADD_and_Update_Resource/Update_NSG.ps1
@@ -18,24 +18,25 @@ $DestinationAddressPrefix = "VirtualNetwork"
 $nsg = Get-AzNetworkSecurityGroup -Name $nsg_name -ResourceGroupName $ResourceGroupName
 
 # 변경할 Inbound rule 작성
-$nsg | Set-AzNetworkSecurityRuleConfig -Name 'HTTP' -Description 'Allow HTTP' `
-    -Access Allow -Protocol Tcp -Direction Inbound -Priority 1000 `
+# rule을 변경할 때는 변경할 Rule Name을 지정한다.
+$nsg | Set-AzNetworkSecurityRuleConfig -Name 'allowAppPort8081' -Description 'Allow Tomcat' `
+    -Access Allow -Protocol Tcp -Direction Inbound -Priority 2000 `
     -SourceAddressPrefix $SourceAddressPrefix -SourcePortRange * `
-    -DestinationAddressPrefix $DestinationAddressPrefix -DestinationPortRange 80
+    -DestinationAddressPrefix $DestinationAddressPrefix -DestinationPortRange 8081
 
-$nsg | Set-AzNetworkSecurityRuleConfig -Name 'HTTPS' -Description 'Allow HTTPS' `
-    -Access Allow -Protocol Tcp -Direction Inbound -Priority 1001 `
-    -SourceAddressPrefix $SourceAddressPrefix -SourcePortRange * `
-    -DestinationAddressPrefix $DestinationAddressPrefix -DestinationPortRange 443
-
-$nsg | Set-AzNetworkSecurityRuleConfig -Name 'SSH' -Description 'Allow SSH' `
-    -Access Allow -Protocol Tcp -Direction Inbound -Priority 1002 `
-    -SourceAddressPrefix $SourceAddressPrefix -SourcePortRange * `
-    -DestinationAddressPrefix $DestinationAddressPrefix -DestinationPortRange 16215
-
-$nsg | Set-AzNetworkSecurityRuleConfig -Name 'MySQL' -Description 'Allow MySQL' `
-    -Access Allow -Protocol Tcp -Direction Inbound -Priority 1003 `
-    -SourceAddressPrefix $SourceAddressPrefix -SourcePortRange * `
-    -DestinationAddressPrefix $DestinationAddressPrefix -DestinationPortRange 3306
+# $nsg | Set-AzNetworkSecurityRuleConfig -Name 'HTTPS' -Description 'Allow HTTPS' `
+#     -Access Allow -Protocol Tcp -Direction Inbound -Priority 1001 `
+#     -SourceAddressPrefix $SourceAddressPrefix -SourcePortRange * `
+#     -DestinationAddressPrefix $DestinationAddressPrefix -DestinationPortRange 443
+#
+# $nsg | Set-AzNetworkSecurityRuleConfig -Name 'SSH' -Description 'Allow SSH' `
+#     -Access Allow -Protocol Tcp -Direction Inbound -Priority 1002 `
+#     -SourceAddressPrefix $SourceAddressPrefix -SourcePortRange * `
+#     -DestinationAddressPrefix $DestinationAddressPrefix -DestinationPortRange 16215
+#
+# $nsg | Set-AzNetworkSecurityRuleConfig -Name 'MySQL' -Description 'Allow MySQL' `
+#     -Access Allow -Protocol Tcp -Direction Inbound -Priority 1003 `
+#     -SourceAddressPrefix $SourceAddressPrefix -SourcePortRange * `
+#     -DestinationAddressPrefix $DestinationAddressPrefix -DestinationPortRange 3306
 # nsg를 업데이트한다.
 $nsg | Set-AzNetworkSecurityGroup
