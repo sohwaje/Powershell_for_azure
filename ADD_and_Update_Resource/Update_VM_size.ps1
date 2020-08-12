57dca195c9bcdd87fa5c181582b30fc5428bf47b
sigongweb <kisssulran@i-screammedia.com>
2020-08-12 13:25:41 +0900

    add variable, fix code

---
+++ ADD_and_Update_Resource/Update_VM_size.ps1
@@ -0,0 +1,23 @@
+################################################################################
+#                         자격 증명을 통해 Azure에 로그인                        #
+################################################################################
+# Login-AzAccount
+# Get-AzSubscription
+# Set-AzContext -SubscriptionId "yourSubscriptionID"
+################################################################################
+#                                   변수 설정                                   #
+################################################################################
+$ResourceGroupName = "ISCREAM"
+$vmName            = "VMname"
+$VmSize            = "Standard_DS3_v2"
+# 0. 사용 가능한 리소스 찾기
+Get-AzVMSize -Location "koreacentral"
+
+# 1. 리소스 그룹명, 가상머신 이름
+$vm = Get-AzVM -ResourceGroupName $ResourceGroupName -VMName $vmName
+
+# 2. 변경할 사이즈 이름
+$vm.HardwareProfile.VmSize = $VmSize
+
+# 3. 사이즈 변경
+Update-AzVM -VM $vm -ResourceGroupName $ResourceGroupName
