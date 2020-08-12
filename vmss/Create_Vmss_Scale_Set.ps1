b1a3b97ed16aa37ff70a4c3023e2ef31d0cb2d45
sigongweb <kisssulran@i-screammedia.com>
2020-08-12 17:03:39 +0900

    create autoscaleset configuration

---
+++ vmss/Create_Vmss_Scale_Set.ps1
@@ -0,0 +1,24 @@
+################################################################################
+#                         자격 증명을 통해 Azure에 로그인                        #
+################################################################################
+# Login-AzAccount
+# Get-AzSubscription
+# Set-AzContext -SubscriptionId "yourSubscriptionID"
+################################# 변수 설정 #####################################
+$Location             = "koreacentral"
+$ResourceGroupName    = "ISCREAM"
+$VMScaleSetName       = "TEST-VM-ScaleSet"
+$vnet_name            = "Hi-Class"
+$subnet_name          = "SEI-Subnet"
+$PublicIpAddressName  = "TEST-VM-PIP"
+$LoadBalancerName     = "TEST-LB"
+
+New-AzVmss `
+  -ResourceGroupName $ResourceGroupName `
+  -Location $Location `
+  -VMScaleSetName $VMScaleSetName `
+  -VirtualNetworkName $vnet_name `
+  -SubnetName $subnet_name `
+  -PublicIpAddressName $PublicIpAddressName  `
+  -LoadBalancerName $LoadBalancerName `
+  -UpgradePolicyMode "Automatic"
