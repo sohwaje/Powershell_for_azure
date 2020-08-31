403b3529fe453e1a0f064433703ca0b57eaf20c2
sigongweb <kisssulran@i-screammedia.com>
2020-08-31 16:56:20 +0900

    update

--- extensions/install_node-export.sh
+++ extensions/install_node-export.sh
@@ -2,10 +2,9 @@
 #ref : https://teamsmiley.github.io/2020/01/17/prometheus/
 # download node_exporter and decompress
 wget -P /tmp/ https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
-tar xvfz node_exporter-1.0.1.linux-amd64.tar.gz
-cd node_exporter-1.0.1.linux-amd64/
-cp node_exporter /usr/local/bin
-cd .. && rm -rf node_exporter*
+tar xvfz /tmp/node_exporter-1.0.1.linux-amd64.tar.gz
+cp /tmp/node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin
+rm -rf /tmp/node_exporter*
 
 # add systemctl service
 bash -c "cat << EOF > /etc/systemd/system/node_exporter.service
