a1a7f0d7cfe31cdb3bc79cc28c0ab6f0d382f9b3
Lee Yusung <yusunglee@Leeui-iMac.local>
2020-09-02 21:15:50 +0900

    update

--- extensions/install.sh
+++ extensions/install.sh
@@ -164,3 +164,28 @@ docker run -d --name=netdata \
   --cap-add SYS_PTRACE \
   --security-opt apparmor=unconfined \
   netdata/netdata
+
+# install prometheus node-exporter
+sudo wget -P /tmp/ https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+sudo tar xvfz /tmp/node_exporter-1.0.1.linux-amd64.tar.gz
+sudo cp /tmp/node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin
+sudo rm -rf /tmp/node_exporter*
+
+# add systemctl service
+sudo bash -c "cat << EOF > /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=root
+Group=root
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF"
+
+# start node_exporter
+sudo systemctl daemon-reload && sudo systemctl start node_exporter && sudo systemctl enable node_exporter
