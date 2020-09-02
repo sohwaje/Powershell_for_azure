debaeee9daca7d5060d426b5746a7d74d6570091
Lee Yusung <yusunglee@Leeui-iMac.local>
2020-09-02 22:21:42 +0900

    dd

--- extensions/install_node-export.sh
+++ extensions/install_node-export.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
-#ref : https://teamsmiley.github.io/2020/01/17/prometheus/
-# download node_exporter and decompress
-wget -P /tmp/ https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
-tar xvfz /tmp/node_exporter-1.0.1.linux-amd64.tar.gz
-cp /tmp/node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin
-rm -rf /tmp/node_exporter*
+# install prometheus node-exporter
+sudo wget -P \
+  /tmp/ https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz; \
+  cd /tmp; \
+  sudo tar xvfz node_exporter-1.0.1.linux-amd64.tar.gz; \
+  sudo cp /tmp/node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin; \
+  sudo rm -rf /tmp/node_exporter*
 
 # add systemctl service
-bash -c "cat << EOF > /etc/systemd/system/node_exporter.service
+sudo bash -c "cat << EOF > /etc/systemd/system/node_exporter.service
 [Unit]
 Description=Node Exporter
 After=network.target
@@ -22,5 +22,6 @@ ExecStart=/usr/local/bin/node_exporter
 WantedBy=multi-user.target
 EOF"
 
+
 # start node_exporter
-systemctl daemon-reload && systemctl start node_exporter && systemctl enable node_exporter
+sudo systemctl daemon-reload && sudo systemctl start node_exporter && sudo systemctl enable node_exporter
