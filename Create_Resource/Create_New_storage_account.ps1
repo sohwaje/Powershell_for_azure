657fa4ab98ad5329248f4624bcd09b29b7e7c7e9
sigongweb <kisssulran@i-screammedia.com>
2020-09-09 09:31:03 +0900

    update

--- Create_Resource/Create_New_storage_account.ps1
+++ Create_Resource/Create_New_storage_account.ps1
@@ -33,30 +33,30 @@ NetworkRulSet Example
 ################################# 변수 설정 ######################################
 $ResourceGroupName        = "ISCREAM"
 $Location                 = "koreacentral"
-$storageaccountName       = "cacheforhiclass"
+$storageaccountName       = "TESTBootDiagnostic"
 $vnet_name                = "Hi-Class"
 
 ################################# 스토리지 계정 생성#################################
-# New-AzStorageAccount `
-#   -ResourceGroupName $ResourceGroupName `
-#   -Name $storageaccountName `
-#   -Location $Location `
-#   -Kind "BlobStorage" `
-#   -SkuName "Standard_LRS" `
-#   -AccessTier Hot
-  # -NetworkRuleSet (@{bypass="Logging,Metrics";
-  # ipRules=(@{IPAddressOrRange="10.1.0.0/24";Action="allow"},
-  #          @{IPAddressOrRange="10.1.3.0/24";Action="allow"},
-  #          @{IPAddressOrRange="10.1.4.0/24";Action="allow"},
-  #          @{IPAddressOrRange="10.1.5.0/24";Action="allow"},
-  #          @{IPAddressOrRange="10.1.12.0/24";Action="allow"},
-  #          @{IPAddressOrRange="10.1.13.0/24";Action="allow"});
-  # virtualNetworkRules=(@{VirtualNetworkResourceId="/subscriptions/64268000-4de0-460d-9cc0-5b7730789327/resourceGroups/ISCREAM/providers/Microsoft.Network/virtualNetworks/Hi-Class/subnets/Hi-Class-Subnet";Action="allow"},
-  #                      @{VirtualNetworkResourceId="/subscriptions/64268000-4de0-460d-9cc0-5b7730789327/resourceGroups/ISCREAM/providers/Microsoft.Network/virtualNetworks/Hi-Class/subnets/Hi-ClassDB-Subnet";Action="allow"},
-  #                      @{VirtualNetworkResourceId="/subscriptions/64268000-4de0-460d-9cc0-5b7730789327/resourceGroups/ISCREAM/providers/Microsoft.Network/virtualNetworks/Hi-Class/subnets/Hi-Class-Push-App-Subnet";Action="allow"},
-  #                      @{VirtualNetworkResourceId="/subscriptions/64268000-4de0-460d-9cc0-5b7730789327/resourceGroups/ISCREAM/providers/Microsoft.Network/virtualNetworks/Hi-Class/subnets/Hi-Class-Push-DB-Subnet";Action="allow"},
-  #                      @{VirtualNetworkResourceId="/subscriptions/64268000-4de0-460d-9cc0-5b7730789327/resourceGroups/ISCREAM/providers/Microsoft.Network/virtualNetworks/Hi-Class/subnets/Hi-Class-Kube";Action="allow"},
-  #                      @{VirtualNetworkResourceId="/subscriptions/64268000-4de0-460d-9cc0-5b7730789327/resourceGroups/ISCREAM/providers/Microsoft.Network/virtualNetworks/Hi-Class/subnets/Hi-Class-Kube-Pro";Action="allow"});
-  # defaultAction="Deny"})
-$subnet = Get-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Name $vnet_name | Get-AzVirtualNetworkSubnetConfig
-Add-AzStorageAccountNetworkRule -ResourceGroupName $ResourceGroupName -Name $storageaccountName -VirtualNetworkResourceId $subnet[13].Id
+New-AzStorageAccount `
+  -ResourceGroupName $ResourceGroupName `
+  -Name $storageaccountName `
+  -Location $Location `
+  -Kind "BlobStorage" `
+  -SkuName "Standard_LRS" `
+  -AccessTier Hot
+  -NetworkRuleSet (@{bypass="Logging,Metrics";
+  ipRules=(@{IPAddressOrRange="10.1.0.0/24";Action="allow"},
+           @{IPAddressOrRange="10.1.3.0/24";Action="allow"},
+           @{IPAddressOrRange="10.1.4.0/24";Action="allow"},
+           @{IPAddressOrRange="10.1.5.0/24";Action="allow"},
+           @{IPAddressOrRange="10.1.12.0/24";Action="allow"},
+           @{IPAddressOrRange="10.1.13.0/24";Action="allow"});
+  virtualNetworkRules=(@{VirtualNetworkResourceId="/subscriptions/64268000-4de0-460d-9cc0-5b7730789327/resourceGroups/ISCREAM/providers/Microsoft.Network/virtualNetworks/Hi-Class/subnets/Hi-Class-Subnet";Action="allow"},
+                       @{VirtualNetworkResourceId="/subscriptions/64268000-4de0-460d-9cc0-5b7730789327/resourceGroups/ISCREAM/providers/Microsoft.Network/virtualNetworks/Hi-Class/subnets/Hi-ClassDB-Subnet";Action="allow"},
+                       @{VirtualNetworkResourceId="/subscriptions/64268000-4de0-460d-9cc0-5b7730789327/resourceGroups/ISCREAM/providers/Microsoft.Network/virtualNetworks/Hi-Class/subnets/Hi-Class-Push-App-Subnet";Action="allow"},
+                       @{VirtualNetworkResourceId="/subscriptions/64268000-4de0-460d-9cc0-5b7730789327/resourceGroups/ISCREAM/providers/Microsoft.Network/virtualNetworks/Hi-Class/subnets/Hi-Class-Push-DB-Subnet";Action="allow"},
+                       @{VirtualNetworkResourceId="/subscriptions/64268000-4de0-460d-9cc0-5b7730789327/resourceGroups/ISCREAM/providers/Microsoft.Network/virtualNetworks/Hi-Class/subnets/Hi-Class-Kube";Action="allow"},
+                       @{VirtualNetworkResourceId="/subscriptions/64268000-4de0-460d-9cc0-5b7730789327/resourceGroups/ISCREAM/providers/Microsoft.Network/virtualNetworks/Hi-Class/subnets/Hi-Class-Kube-Pro";Action="allow"});
+  defaultAction="Deny"})
+# $subnet = Get-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Name $vnet_name | Get-AzVirtualNetworkSubnetConfig
+# Add-AzStorageAccountNetworkRule -ResourceGroupName $ResourceGroupName -Name $storageaccountName -VirtualNetworkResourceId $subnet[13].Id
