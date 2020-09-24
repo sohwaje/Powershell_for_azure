8cef311fdc96afe3a48fb76a1a291f9a715a042f
sigongweb <kisssulran@i-screammedia.com>
2020-09-24 15:08:40 +0900

    get all vms info

---
+++ info/Collect_details_about_all_VMs.ps1
@@ -0,0 +1,37 @@
+<#
+.Description
+구독에 있는 모든 VM의 자세한 정보를 가져온다.
+#>
+#Provide the subscription Id where the VMs reside
+$subscriptionId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+#Provide the name of the csv file to be exported
+$reportName = "myReport.csv"
+################################################################################
+Select-AzSubscription $subscriptionId
+$report = @()
+$vms = Get-AzVM
+$publicIps = Get-AzPublicIpAddress
+$nics = Get-AzNetworkInterface | ?{ $_.VirtualMachine -NE $null}
+foreach ($nic in $nics) {
+    $info = "" | Select VmName, ResourceGroupName, Region, VmSize, VirtualNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress, NicName, ApplicationSecurityGroup
+    $vm = $vms | ? -Property Id -eq $nic.VirtualMachine.id
+    foreach($publicIp in $publicIps) {
+        if($nic.IpConfigurations.id -eq $publicIp.ipconfiguration.Id) {
+            $info.PublicIPAddress = $publicIp.ipaddress
+            }
+        }
+        $info.OsType = $vm.StorageProfile.OsDisk.OsType
+        $info.VMName = $vm.Name
+        $info.ResourceGroupName = $vm.ResourceGroupName
+        $info.Region = $vm.Location
+        $info.VmSize = $vm.HardwareProfile.VmSize
+        $info.VirtualNetwork = $nic.IpConfigurations.subnet.Id.Split("/")[-3]
+        $info.Subnet = $nic.IpConfigurations.subnet.Id.Split("/")[-1]
+        $info.PrivateIpAddress = $nic.IpConfigurations.PrivateIpAddress
+        $info.NicName = $nic.Name
+        $info.ApplicationSecurityGroup = $nic.IpConfigurations.ApplicationSecurityGroups.Id
+        $report+=$info
+    }
+$report | ft VmName, ResourceGroupName, Region, VmSize, VirtualNetwork, Subnet, PrivateIpAddress, OsType, PublicIPAddress, NicName, ApplicationSecurityGroup
+$report | Export-CSV "$home/$reportName"
