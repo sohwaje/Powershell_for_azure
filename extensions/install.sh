49cda4cb7dde145738a7fd91488cdc54f4484b09
Lee Yusung <yusunglee@Leeui-iMac.local>
2020-11-02 20:50:13 +0900

    sudo chown -R /home/azureuser/apps

--- extensions/install.sh
+++ extensions/install.sh
@@ -192,6 +192,7 @@ EOF"
 
 # create makedir
 sudo mkdir /home/azureuser/apps
+sudo chown -R /home/azureuser/apps
 # start node_exporter
 sudo systemctl daemon-reload && sudo systemctl start node_exporter && sudo systemctl enable node_exporter
 
