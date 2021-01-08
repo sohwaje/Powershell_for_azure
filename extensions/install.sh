86a290e75b2406088b32192d45284559e81a25b2
sohwaje <kissenyo@gmail.com>
2021-01-08 11:25:05 +0900

    update

--- extensions/install.sh
+++ extensions/install.sh
@@ -153,50 +153,37 @@ sudo curl https://bootstrap.pypa.io/get-pip.py | python
 # install and import python3 psutil
 sudo python3 -m pip install -U psutil
 
-# fluentd docker image pull
-docker pull fluent/fluentd:v0.12-debian
-
-# Install a bashtop(resource monitoring)
-sudo git clone https://github.com/aristocratos/bpytop.git && cd bpytop;sudo make install
-sudo ln -s /usr/local/bin/bpytop /usr/bin/
-
-# sudo sed -i 's/^Listen 80$/Listen 38080/' /etc/httpd/conf/httpd.conf
-# sudo sed -i 's/Listen 38080/Listen 80/' /etc/httpd/conf/httpd.conf
-# sudo echo "Test-Page" > /var/www/html/index.html
-# sudo systemctl start httpd
-# sudo systemctl enable httpd
-
 # install prometheus node-exporter
-sudo wget -P \
-  /tmp/ https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz; \
-  cd /tmp; \
-  sudo tar xvfz node_exporter-1.0.1.linux-amd64.tar.gz; \
-  sudo cp /tmp/node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin; \
-  sudo rm -rf /tmp/node_exporter*
-
-# add systemctl service
-sudo bash -c "cat << EOF > /etc/systemd/system/node_exporter.service
-[Unit]
-Description=Node Exporter
-After=network.target
-
-[Service]
-User=root
-Group=root
-Type=simple
-ExecStart=/usr/local/bin/node_exporter
-
-[Install]
-WantedBy=multi-user.target
-EOF"
+# sudo wget -P \
+#   /tmp/ https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz; \
+#   cd /tmp; \
+#   sudo tar xvfz node_exporter-1.0.1.linux-amd64.tar.gz; \
+#   sudo cp /tmp/node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin; \
+#   sudo rm -rf /tmp/node_exporter*
+#
+# # add systemctl service
+# sudo bash -c "cat << EOF > /etc/systemd/system/node_exporter.service
+# [Unit]
+# Description=Node Exporter
+# After=network.target
+#
+# [Service]
+# User=root
+# Group=root
+# Type=simple
+# ExecStart=/usr/local/bin/node_exporter
+#
+# [Install]
+# WantedBy=multi-user.target
+# EOF"
 
 # create makedir
-sudo mkdir /home/azureuser/apps
-sudo chown -R azureuser.azureuser /home/azureuser/apps
+# sudo mkdir /home/azureuser/apps
+# sudo chown -R azureuser.azureuser /home/azureuser/apps
 # start node_exporter
-sudo systemctl daemon-reload && sudo systemctl start node_exporter && sudo systemctl enable node_exporter
+# sudo systemctl daemon-reload && sudo systemctl start node_exporter && sudo systemctl enable node_exporter
 
 # prometheus reload
-sudo curl -X POST http://10.1.12.6:9090/-/reload
+# sudo curl -X POST http://10.1.12.6:9090/-/reload
 
 # slack_message "$HOSTNAME 가상 머신을 생성하였습니다." true
