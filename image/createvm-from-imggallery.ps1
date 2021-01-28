d38c3afc1dc97f3d4b9c1d3b03168ebdcf7dec28
sohwaje <kissenyo@gmail.com>
2021-01-28 20:06:59 +0900

    update

--- image/createvm-from-imggallery.ps1
+++ image/createvm-from-imggallery.ps1
@@ -21,7 +21,7 @@ $sourceVm = Get-AzVM `
    -ResourceGroupName $myResourceGroupName
 
 ### Deprovision은 고유 UUID를 삭제한다
-#   sudo waagent -deprovision+user
+#   sudo waagent -deprovision
 ###
 
 # VM을 중지
@@ -31,17 +31,17 @@ Stop-AzVM `
   -Force
 
 # 이미지 정의 만들기 : 새로운 이미지를 만들 때 Publisher, Offer, Sku는 변경해야 한다.
-# -OsState specialized
+# -OsState generalized
 $imageDefinition = New-AzGalleryImageDefinition `
    -GalleryName $gallery_name `
    -ResourceGroupName $gallery_ResourceGroupName `
    -Location $gallery_location     `
-   -Name 'vmssImagev2-Definition' `
-   -OsState generalized `
+   -Name 'vmssImagev4-Definition' `
+   -OsState specialized `
    -OsType Linux `
-   -Publisher 'OpenLogicv2' `
-   -Offer 'CentOSv2' `
-   -Sku '7.7v2'
+   -Publisher 'OpenLogicv4' `
+   -Offer 'CentOSv4' `
+   -Sku '7.7v4'
 
 # 이미지 버전 만들기
 $region1 = @{Name='koreacentral';ReplicaCount=1}
@@ -49,7 +49,7 @@ $targetRegions = @($region1)
 
 $job = $imageVersion = New-AzGalleryImageVersion `
    -GalleryImageDefinitionName $imageDefinition.Name`
-   -GalleryImageVersionName '2.0.0' `
+   -GalleryImageVersionName '4.0.0' `
    -GalleryName $gallery_name `
    -ResourceGroupName $gallery_ResourceGroupName `
    -Location $gallery_location `
@@ -57,3 +57,6 @@ $job = $imageVersion = New-AzGalleryImageVersion `
    -SourceImageId $sourceVm.Id.ToString() `
    -PublishingProfileEndOfLifeDate '2021-12-01' `
    -asJob
+
+# 작업 진행률 보기
+$job.State
