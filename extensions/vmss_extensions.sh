e1aa4165eeda5b64b8c79c5c04751c96ed742232
sohwaje <kissenyo@gmail.com>
2021-01-31 10:49:45 +0900

    update

--- extensions/vmss_extensions.sh
+++ extensions/vmss_extensions.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget https://github.com/sohwaje/Powershell_for_azure/blob/master/extensions/hello-0.0.3-SNAPSHOT.jar -P /tmp
+wget https://github.com/sohwaje/Powershell_for_azure/raw/master/extensions/hello-0.0.3-SNAPSHOT.jar -P /tmp
 sleep 1
 java -jar /tmp/hello-0.0.3-SNAPSHOT.jar
