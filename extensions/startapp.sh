175b3c6c186d105832e26a514a21a53d64e5f8d9
sohwaje <kissenyo@gmail.com>
2021-03-12 08:04:13 +0900

    update

--- extensions/startapp.sh
+++ extensions/startapp.sh
@@ -1,3 +1,3 @@
 wget -P /tmp/ https://github.com/sohwaje/Powershell_for_azure/blob/master/extensions/hello-0.0.3-SNAPSHOT.jar
 
-nohup java -jar hello-0.0.3-SNAPSHOT.jar & 
+cd /tmp && nohup java -jar hello-0.0.3-SNAPSHOT.jar & 
