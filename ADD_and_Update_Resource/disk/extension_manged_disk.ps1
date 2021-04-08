cd252b0e2ac7125e15280a180b01d0266d4c1fde
sohwaje <kissenyo@gmail.com>
2021-04-08 16:16:57 +0900

    d

--- ADD_and_Update_Resource/disk/extension_manged_disk.ps1
+++ ADD_and_Update_Resource/disk/extension_manged_disk.ps1
@@ -2,6 +2,6 @@ az vm deallocate --resource-group stg-business --name smartclass
 
 az disk list --resource-group stg-business --query '[*].{Name:name,Gb:diskSizeGb,Tier:accountType}' --output table
 
-az disk update --resource-group stg-business --name SMARTclass_OsDisk_1_69a534dfd9f449bcb226db3dba92ef67 --size-gb 100
+az disk update --resource-group stg-business --name SMARTclass_DataDisk_0 --size-gb 1000
 
 az vm start --resource-group stg-business --name smartclass
