c370eb1f39fc81c4d40eee094a88a9838de0d40b
sohwaje <kissenyo@gmail.com>
2021-04-13 15:42:39 +0900

    update

--- ADD_and_Update_Resource/disk/extension_manged_disk.ps1
+++ ADD_and_Update_Resource/disk/extension_manged_disk.ps1
@@ -1,7 +1,7 @@
-az vm deallocate --resource-group stg-business --name smartclass
+az vm deallocate --resource-group ISCREAM --name TEST
 
-az disk list --resource-group stg-business --query '[*].{Name:name,Gb:diskSizeGb,Tier:accountType}' --output table
+az disk list --resource-group ISCREAM --query '[*].{Name:name,Gb:diskSizeGb,Tier:accountType}' --output table
 
-az disk update --resource-group stg-business --name SMARTclass_DataDisk_0 --size-gb 1000
+az disk update --resource-group ISCREAM --name TEST --size-gb 100
 
-az vm start --resource-group stg-business --name smartclass
+az vm start --resource-group ISCREAM --name TEST
