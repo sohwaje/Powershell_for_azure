3bf44c4f7c429380012e9067e0876506349ee4ac
sohwaje <kissenyo@gmail.com>
2021-08-03 10:07:19 +0900

    update

--- extensions/install.sh
+++ extensions/install.sh
@@ -153,26 +153,26 @@ sudo curl https://bootstrap.pypa.io/get-pip.py | python
 sudo python3 -m pip install -U psutil
 
 # install prometheus node-exporter
-# sudo wget -P \
-#   /tmp/ https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz; \
-#   cd /tmp; \
-#   sudo tar xvfz node_exporter-1.0.1.linux-amd64.tar.gz; \
-#   sudo cp /tmp/node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin; \
-#   sudo rm -rf /tmp/node_exporter*
+sudo wget -P \
+  /tmp/ https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz; \
+  cd /tmp; \
+  sudo tar xvfz node_exporter-1.0.1.linux-amd64.tar.gz; \
+  sudo cp /tmp/node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin; \
+  sudo rm -rf /tmp/node_exporter*
 #
 # # add systemctl service
-# sudo bash -c "cat << EOF > /etc/systemd/system/node_exporter.service
-# [Unit]
-# Description=Node Exporter
-# After=network.target
-# [Service]
-# User=root
-# Group=root
-# Type=simple
-# ExecStart=/usr/local/bin/node_exporter
-# [Install]
-# WantedBy=multi-user.target
-# EOF"
+sudo bash -c "cat << EOF > /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+After=network.target
+[Service]
+User=root
+Group=root
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+[Install]
+WantedBy=multi-user.target
+EOF"
 
 # create makedir
 # sudo mkdir /home/azureuser/apps
